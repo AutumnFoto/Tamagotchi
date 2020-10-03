aecf3c1fb22e60bbb98b0107d5a8a075789d55b7
Autumn Fotopoulos <acarte94@gmail.com>
2020-10-03 18:54:55 -0500

    fight quad

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,13 @@
 import '../styles/main.scss';
 import eatQuad from './components/eatQuad';
 import playQuad from './components/playQuad';
+import fightQuad from './components/fightQuad';
 
 const init = () => {
   eatQuad.eatQuad();
   playQuad.playQuad();
+  fightQuad.fightQuad();
+
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
   console.warn('YOU ARE UP AND RUNNING!');
 };
