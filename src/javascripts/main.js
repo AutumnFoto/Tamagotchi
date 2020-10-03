8e4fa1e2dba529a9caa0c76b50a6208eb37c9ca9
Autumn Fotopoulos <acarte94@gmail.com>
2020-10-03 18:52:09 -0500

    play quad

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import eatQuad from './components/eatQuad';
+import playQuad from './components/playQuad';
 
 const init = () => {
   eatQuad.eatQuad();
+  playQuad.playQuad();
   $('#app').html('<h1>HELLO! You are up and running!</h1>');
-  console.log('YOU ARE UP AND RUNNING!');
+  console.warn('YOU ARE UP AND RUNNING!');
 };
 
 init();
